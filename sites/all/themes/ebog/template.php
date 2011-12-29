606bab1ee133dbf4f8c029b54d57e76d7b5f1261
Martin Cording <mc@inleadmedia.com>
2011-12-29 13:59:11 +0100

    Transfer changes from netsound2.

--- sites/all/themes/ebog/template.php
+++ sites/all/themes/ebog/template.php
@@ -1,20 +1,4 @@
 <?php
-// $Id: template.php,v 1.1.2.9 2010/07/09 14:53:42 himerus Exp $
-
-/*
- * Add any conditional stylesheets you will need for this sub-theme.
- *
- * To add stylesheets that ALWAYS need to be included, you should add them to
- * your .info file instead. Only use this section if you are including
- * stylesheets based on certain conditions.  */
-/* -- Delete this line if you want to use and modify this code
-// Example: optionally add a fixed width CSS file.
-if (theme_get_setting('ebog_fixed')) {
-  drupal_add_css(path_to_theme() . '/layout-fixed.css', 'theme', 'all');
-}
-// */
-
-
 /**
  * Implementation of HOOK_theme().
  */
@@ -28,27 +12,36 @@ function ebog_theme(&$existing, $type, $theme, $path) {
   return $hooks;
 }
 
+function fetchNewestComment($node = false, $count = 1) {
+  $sql = db_query_range("
+    SELECT
+      c.*
+    FROM
+      {comments} AS c
+    WHERE
+      c.nid = %d
+  ", $node->nid, 0, $count);
+    $result = db_fetch_object($sql);
+    
+    return $result;
+}
+
 /**
- * Override or insert variables into all templates.
+ * Fetch all terms from specified vocabulary
  *
- * @param $vars
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered (name of the .tpl.php file.)
- */
-//function ebog_preprocess(&$vars, $hook) {
-  //fb($vars,$hook);
-  //$vars['sample_variable'] = t('Lorem ipsum.');
-//}
-//function ebog_preprocess_views_view(&$vars) {
-  //switch ($vars['name']) {
-    //case 'anmeldelser':
-      //fb($vars,'vars');
-      //break;
-    //default:
-      //break;
-  //}
-//}
+ * @param Object $node
+ * @param Integer $vid
+ * @return Arrau
+ * @author Hasse R. Hansen
+ **/
+function showEntriesFromVocab($node, $vid) {
+  $entries = array();
+  $items = taxonomy_node_get_terms_by_vocabulary($node, $vid);
+  foreach ($items as $value) {
+    $entries[] = $value->name;
+  }
+  return $entries;
+}
 
 /**
  * Override or insert variables into the page templates.
@@ -89,20 +82,6 @@ function ebog_preprocess_page(&$vars, $hook) {
 }
 
 /**
- * Override or insert variables into the node templates.
- *
- * @param $vars
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("node" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function ebog_preprocess_node(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
-}
-// */
-
-/**
  * Override or insert variables into the comment templates.
  *
  * @param $vars
@@ -110,25 +89,30 @@ function ebog_preprocess_node(&$vars, $hook) {
  * @param $hook
  *   The name of the template being rendered ("comment" in this case.)
  */
-/* -- Delete this line if you want to use this function
-function ebog_preprocess_comment(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function ebog_preprocess_comment(&$variables) {
+  $comment = $variables['comment'];
+  $node = $variables['node'];
+  
+  $uA = elib_user_get_cred($comment->uid);
+  
+  $variables['author']    = $uA['user'];//theme('username', $comment);
+  $variables['content']   = $comment->comment;
+  $variables['date']      = format_date($comment->timestamp);
+ // $variables['links']     = isset($variables['links']) ? theme('links', $variables['links']) : '';
+  $variables['new']       = $comment->new ? t('new') : '';
+  $variables['picture']   = theme_get_setting('toggle_comment_user_picture') ? theme('user_picture', $comment) : '';
+  $variables['signature'] = $comment->signature;
+  $variables['submitted'] = theme('comment_submitted', $comment);
+  $variables['title']     = l($comment->subject, $_GET['q'], array('fragment' => "comment-$comment->cid"));
+  $variables['template_files'][] = 'comment-'. $node->type;
+  // set status to a string representation of comment->status.
+  if (isset($comment->preview)) {
+    $variables['status']  = 'comment-preview';
+  }
+  else {
+    $variables['status']  = ($comment->status == COMMENT_NOT_PUBLISHED) ? 'comment-unpublished' : 'comment-published';
+  }
 }
-// */
-
-/**
- * Override or insert variables into the block templates.
- *
- * @param $vars
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("block" in this case.)
- */
-/* -- Delete this line if you want to use this function
-function ebog_preprocess_block(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
-}
-// */
 
 
 /**
