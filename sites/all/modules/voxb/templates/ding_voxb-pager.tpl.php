247b229e6b8137c68f4eb9351c51314f09f69533
Martin Cording <mc@inleadmedia.com>
2012-03-02 14:34:08 +0100

    Merge with eReolen changes.

--- sites/all/modules/voxb/templates/ding_voxb-pager.tpl.php
+++ sites/all/modules/voxb/templates/ding_voxb-pager.tpl.php
@@ -8,7 +8,7 @@
 ?>
 
 <ul>
-  <li class="prev-page"><?php print l('<<', 'voxb/ajax/seek/' . $data['faust_number'] . '/' . $data['previous']); ?></li>
+  <li class="prev-page"><?php print l('«', 'voxb/ajax/seek/' . $data['faust_number'] . '/' . $data['previous']); ?></li>
   <?php print $data['leaves']; ?>
-  <li class="next-page"><?php print l('>>', 'voxb/ajax/seek/' . $data['faust_number'] . '/' . $data['next']); ?></li>
+  <li class="next-page"><?php print l('»', 'voxb/ajax/seek/' . $data['faust_number'] . '/' . $data['next']); ?></li>
 </ul>
