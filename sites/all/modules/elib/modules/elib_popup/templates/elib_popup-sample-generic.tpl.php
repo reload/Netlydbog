8e822d17cd0e4b4bd4565ef84ac89b434a2249e6
Dragan <di@inlead.dk>
2012-03-28 04:38:57 +0300

    case 183 - Added templates for sample functionality.

---
+++ sites/all/modules/elib/modules/elib_popup/templates/elib_popup-sample-generic.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<h4>
+  <?php echo t('You are now listening to a taste of the book')?> - 
+  <?php echo $data['title']?>
+</h4>
+<div id="audio-player">
+  <?php echo t('Since your browser does not support flash, we can not play the teaser for you, but you can download it')?>
+  <?php echo l(t('here'), $data['link'])?>
+  <?php echo t('And even play it on your computer')?>.
+</div>
