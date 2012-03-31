14affc6f1e36cd32489c388cce8bc08c00e9db11
INLEAD <info@inlead.dk>
2012-03-31 13:54:31 +0000

    Added read more link.

--- sites/all/modules/voxb/js/voxb.item.js
+++ sites/all/modules/voxb/js/voxb.item.js
@@ -267,7 +267,7 @@
             html += '<br />';
             html += '<p><input type="text" name="alias-name" value="" /></p>';
             html += '<br />';
-            html += '<p><input type="checkbox" id="voxb-terms" class="voxb-terms" /><label for="voxb-terms">' + Drupal.t('I accept to provide my certain credentials to 3-rd party services, such as VoxB.') + '</label></p>';
+            html += '<p><input type="checkbox" id="voxb-terms" class="voxb-terms" /><label for="voxb-terms">' + Drupal.t('I accept to provide my certain credentials to 3-rd party services, such as VoxB') + ' (<a href="/brugerskabt">' + Drupal.t('read more') + '</a>).' +'</label></p>';
             html += '</div>';
             $('body').append(html);
 
