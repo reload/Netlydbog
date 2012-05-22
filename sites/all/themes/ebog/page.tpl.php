ebad9d6b316569503773b9eb941ca7d4fc1116d9
AnatolyMuntean <am@inlead.dk>
2012-05-22 14:18:05 +0400

    case 347 - User scalability settings.

--- sites/all/themes/ebog/page.tpl.php
+++ sites/all/themes/ebog/page.tpl.php
@@ -8,7 +8,8 @@
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=0.5, 
+maximum-scale=3.0" />
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <link rel="apple-touch-icon" href="images/ereolen.png" />
